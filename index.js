c54036dc8282359dc562e648bb5fddc0bbb66023
Afanasii Kurakin <afanasy@gmail.com>
2015-11-09 17:43:46 +0800

    added code, README, tests

---
+++ index.js
@@ -0,0 +1,86 @@
+var
+  _ = require('underscore'),
+  format = require('sqlstring').format,
+  escapeId = require('sqlstring').escapeId,
+  set = require('db3-set'),
+  where = require('db3-where'),
+  orderBy = require('db3-order-by')
+
+var app = module.exports = {
+  set: set,
+  where: where,
+  orderBy: orderBy,
+  stringify: function (d, value) {
+    if (_.isString(d))
+      return format(d, value)
+    if (_.isFunction(app.stringify[d.name]))
+      return app.stringify[d.name](d)
+    return String(d)
+  }
+}
+
+_.extend(app.stringify, {
+  createTable: function (d) {
+    if (d.like)
+      return format('create table ?? like ??', [d.table, d.like])
+    if (!_.size(d.field))
+      d.field = ['id', 'name']
+    d.field = _.map(d.field, function (field) {
+      var type = 'text'
+      if (field == 'id')
+        type = 'bigint primary key auto_increment'
+      if (field.match(/Id$/))
+        type = 'bigint'
+      return escapeId(field) + ' ' + type
+    }).join(', ')
+    return 'create table ' + escapeId(d.table) + ' (' + d.field + ')'
+  },
+  dropTable: function (d) {
+    return format('drop table ??', d.table)
+  },
+  truncateTable: function (d) {
+    return format('truncate table ??', d.table)
+  },
+  renameTable: function (d) {
+    return format('rename table ?? to ??', [d.table, d.to])
+  },
+  alterTable: function (d) {
+    return format('alter table ?? drop ??', [d.table, d.drop])
+  },
+  insert: function (d) {
+    if (d.select)
+      return 'insert ' + escapeId(d.table) + ' ' + app.stringify.select(d.select)
+    var query = 'insert ' + escapeId(d.table) + ' set ' + set.query(d.set)
+    if (d.update)
+      query += ' on duplicate key update ' + set.query(d.update)
+    return query
+  },
+  update: function (d) {
+    return format('update ?? set ', d.table) + set.query(d.set) + ' where ' + where.query(d.where)
+  },
+  delete: function (d) {
+    return 'delete from ' + escapeId(d.table) + ' where ' + where.query(d.where)
+  },
+  select: function (d) {
+    if (_.isString(d))
+      d = {table: d}
+    d.field = d.field || '*'
+    if (_.isString(d.field))
+      d.field = [d.field]
+    d.field = _.map(d.field, function (d) {
+      if (d != '*')
+        return escapeId(d)
+      return d
+    }).join(', ')
+    var query = 'select ' + d.field + ' from ' + escapeId(d.table)
+    d.where = where.query(d.where)
+    if (d.where)
+      query += ' where ' + d.where
+    d.orderBy = orderBy.query(d.orderBy)
+    if (d.orderBy)
+      query += ' order by ' + d.orderBy
+    if (d.limit)
+      query += ' limit ' + +d.limit
+    return query
+  }
+})
---
+++ test.js
@@ -0,0 +1,29 @@
+var
+  _ = require('underscore'),
+  expect = require('expect.js'),
+  s = require('./').stringify
+
+var query = {
+  'create table `person` (`id` bigint primary key auto_increment, `name` text)': {name: 'createTable', table: 'person'},
+  'drop table `person`': {name: 'dropTable', table: 'person'},
+  'truncate table `person`': {name: 'truncateTable', table: 'person'},
+  'rename table `person` to `nosrep`': {name: 'renameTable', table: 'person', to: 'nosrep'},
+  'alter table `person` drop `name`': {name: 'alterTable', table: 'person', drop: 'name'},
+  'insert `person` select * from `nosrep`': {name: 'insert', table: 'person', select: 'nosrep'},
+  'insert `person` set `id` = 1, `name` = \'Bob\'': {name: 'insert', table: 'person', set: {id: 1, name: 'Bob'}},
+  'insert `person` set `name` = \'Bob\' on duplicate key update `name` = \'Alice\'': {name: 'insert', table: 'person', set: {name: 'Bob'}, update: {name: 'Alice'}},
+  'update `person` set `name` = \'Alice\' where `id` = 1': {name: 'update', table: 'person', set: {name: 'Alice'}, where: 1},
+  'update `person` set `name` = \'Alice\' where `name` = \'Bob\'': {name: 'update', table: 'person', set: {name: 'Alice'}, where: {name: 'Bob'}},
+  'delete from `person` where `id` = 1': {name: 'delete', table: 'person', where: 1},
+  'delete from `person` where `name` = \'Alice\'': {name: 'delete', table: 'person', where: {name: 'Alice'}}
+}
+
+describe('#queryString', function () {
+  describe('#stringify', function () {
+    _.each(query, function (value, key) {
+      it('does ' + value.name, function () {
+        expect(s(value)).to.be.equal(key)
+      })
+    })
+  })
+})
